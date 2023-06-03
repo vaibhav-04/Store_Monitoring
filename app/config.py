e2eb45f9c5337cb6a68ea3d9a68028d9c0dc5746
Vaibhav Varshney <56710232+vaibhav-04@users.noreply.github.com>
2023-06-03 08:49:56 +0530

    f

--- app/config.py
+++
@@ -1,7 +0,0 @@
-class Config:
-    # SQLALCHEMY_DATABASE_URI = 'sqlite:////tmp/restaurant_monitoring.db'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///restaurant_monitoring.db'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-     
-    #  SQLALCHEMY_DATABASE_URI =postgresql://scott:tiger@localhost/mydatabase
-    
